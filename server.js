98fe6be07ea4657af4cdecf01acc796a10f6849b
djkarmi <sz.lagiewka@gmail.com>
2020-12-14 18:45:04 +0100

    add project files

--- server.js
+++ server.js
@@ -5,20 +5,11 @@ const app = express();
 app.use(express.static(__dirname + '/dist'))
 
 app.get('/', (req, res) => {
-<<<<<<< HEAD
-	res.sendFile('index.html', { root: __dirname });
-=======
     res.sendFile('index.html', { root: __dirname });
->>>>>>> dc005fa3095047c5f7702cbdf885fe67c279f1e4
 });
 
 let port = process.env.PORT || 5000;
 app.listen(port, () => {
-<<<<<<< HEAD
-	console.log(__dirname);
-	console.log("Listening Port " + port);
-=======
     console.log(__dirname);
     console.log("Listening Port " + port);
->>>>>>> dc005fa3095047c5f7702cbdf885fe67c279f1e4
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,6 @@
 module.exports = {
 	entry: [
-<<<<<<< HEAD
-		'./src/main.js'
-=======
 		'./main.js'
->>>>>>> dc005fa3095047c5f7702cbdf885fe67c279f1e4
 	],
 	output: {
 		filename: 'build.js'
