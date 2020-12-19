b53373e047efe769cdd733983f617b3969ef59d7
djkarmi <sz.lagiewka@gmail.com>
2020-12-20 00:00:58 +0100

    dodanie kilku poprawek

--- src/components/app.js
+++ src/components/app.js
@@ -23,6 +23,14 @@ class App extends React.Component {
 
 
     signFuncWAVES() {
+        console.log('text box')
+        var x = document.getElementById("ilosc").value;
+        console.log(x)
+        console.log('koniec')
+
+        //var zmienna = prompt("Ile Szczepan chcesz kupić");
+
+
         const txData = {
             type: 16,
             data: {
@@ -34,28 +42,43 @@ class App extends React.Component {
                 call: {
                     function: 'Zrobprzelew',
                     args: []
-                }, payment: [{ assetId: "WAVES", tokens: 1 }]
+                }, payment: [{ assetId: "WAVES", tokens: x }]
             }
         };
 
 
         if (WavesKeeper) {
+
+
             WavesKeeper.signAndPublishTransaction(txData).then((data) => {
-            // published tx result
+
+
+
+                // published tx result
             }).catch((error) => {
-                //processing errors
+                console.error(error);//processing errors
             });
         }
         else {
             alert("Do tej wymiany potrzebne jest zainstalowanie WavesKeeper");
 
         }
+
+        //alert("Do tej wymiany potrzebne jest zainstalowanie WavesKeeper");
+
+
+
+
+
     }
 
 
 
 
     signFuncSzczepan() {
+
+        var y = document.getElementById("iloscszczepan").value;
+
         const txData = {
             type: 16,
             data: {
@@ -67,16 +90,27 @@ class App extends React.Component {
                 call: {
                     function: 'Zrobprzelew',
                     args: []
-                }, payment: [{ assetId: "GoJ7x4boYrFMcnffZ2kip4mgGsFy4KyZryG9jooLtGXo", tokens: 1 }]
+                }, payment: [{ assetId: "GoJ7x4boYrFMcnffZ2kip4mgGsFy4KyZryG9jooLtGXo", tokens: y }]
             }
         };
 
 
+
+
         if (WavesKeeper) {
             WavesKeeper.signAndPublishTransaction(txData).then((data) => {
-            // published tx result
+                // published tx result
+
+
+                console.log(signAndPublishTransaction); //displaying the result on the console
+
+
+
+
+
+
             }).catch((error) => {
-                //processing errors
+                console.error(error);//processing errors
             });
         }
         else {
@@ -86,9 +120,12 @@ class App extends React.Component {
     }
 
 
+
+
     authFunc() {
         const authData = { data: "Autoryzacja danych" };
         if (WavesKeeper) {
+
             WavesKeeper.auth(authData)
                 .then(auth => {
                     console.log(auth); //displaying the result on the console
@@ -102,18 +139,123 @@ class App extends React.Component {
         }
     }
 
+    funcvalue(e) {
+        var xy = document.getElementById("ilosc").value;
+        var xyz = document.getElementById("wynik");
+
+
+        return xy;
+    }
+
+
+    zmiana(e) {
+        var t;
+
+
+        ("" === (t = e.target.value.replace(/,/g, ".")) || Bg.test(t.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"))) && n(t)
+    }
+
+    zmiana2(e) {
+        let s = document.getElementById("ilosc").value;
+        s = s * 100;
+        s = Math.floor(s);
+        s = s / 100;
+        console.log(s);
+        s = s * 0.97;
+        console.log(s);
+        s = s * 100;
+        s = Math.floor(s);
+        s = s / 100;
+        console.log(s);
+        document.getElementById("wynik").value = s;
+
+    }
+
+
+    zmiana3(e) {
+        let s = document.getElementById("wynik").value * 100;
+        s = s / 97;
+        s = s * 100;
+        s = Math.ceil(s)
+        s = s / 100;
+        document.getElementById("ilosc").value = s;
+        let s1 = document.getElementById("wynik").value * 100;
+        s1 = Math.round(s1)
+        s1 = s1 / 100;
+        document.getElementById("wynik").value = s1;
+
+    }
+
+    zmiana4(e) {
+        let s = document.getElementById("ilosc2").value;
+        s = s * 100;
+        s = Math.floor(s);
+        s = s / 100;
+        console.log(s);
+        s = s * 0.97;
+        console.log(s);
+        s = s * 100;
+        s = Math.floor(s);
+        s = s / 100;
+        console.log(s);
+        document.getElementById("wynik2").value = s;
+
+    }
+
+
+    zmiana5(e) {
+        let s = document.getElementById("wynik2").value * 100;
+        s = s / 97;
+        s = s * 100;
+        s = Math.ceil(s)
+        s = s / 100;
+        document.getElementById("ilosc2").value = s;
+        let s1 = document.getElementById("wynik2").value * 100;
+        s1 = Math.round(s1)
+        s1 = s1 / 100;
+        document.getElementById("wynik2").value = s1;
+
+    }
 
 
     render() {
         return (
             <div className="container">
-                <input className="btn btn-primary" type="submit" value="Wymień WAVES na Szczepan" onClick={this.signFuncWAVES} />
-                <br></br>
-                <br></br>
 
-                <input className="btn btn-primary" type="submit" value="Wymień Szczepan na Waves " onClick={this.signFuncSzczepan} />
+                <table algin="center">
+                    <tr>
+                        <td align="center" width="900">
 
-            </div>
+
+
+
+                            <input name="arg1" id="ilosc" width="1200" height="600" inputmode="decimal" title="Ilość WAVES" autocomplete="off" autocorrect="off" type="number" placeholder="0.00000000" minlength="1" maxlength="79" spellcheck="false" pattern="^[0-9]*[.,]?[0-9]*$" onChange={this.zmiana2}></input>
+
+
+                            <input name="otrzymasz" id="wynik" placeholder="0.00" type="number"  itle="Ilość Szczepanów" onChange={this.zmiana3}></input> <p></p>
+                            <br></br>
+
+
+                            <input className="btn btn-primary" width="600" height="0" type="submit" value="Wymień WAVES na Szczepan" onClick={this.signFuncWAVES} />
+                        </td>
+                        <td align="center" width="900">
+
+
+
+
+                            <input name="arg1" id="ilosc2" inputmode="decimal" title="Ilość WAVES" autocomplete="off" autocorrect="off" type="number" placeholder="0.00" minlength="1" maxlength="79" spellcheck="false" pattern="^[0-9]*[.,]?[0-9]*$" onChange={this.zmiana5}></input>
+
+
+                            <input name="otrzymasz" id="wynik2" placeholder="0.00000000" type="number" onChange={this.zmiana4} ></input> <p></p>
+                            <br></br>
+
+
+
+                            <input className="btn btn-primary" type="submit" value="Wymień Szczepan na Waves " onClick={this.signFuncSzczepan} />
+                        </td>
+                    </tr>
+                </table>
+            </div >
         )
     }
 }
